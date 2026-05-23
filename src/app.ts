4f8cf38920133a3e09d47c298f96eda54e751806
Md Al Amin <tanviralamin221@gmail.com>
2026-05-24 02:35:59 +0600

    making read me

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import type { Request, Response } from "express";
+import cors from "cors";
 
 import express from "express";
 import { authRouter } from "./modules/auth/auth.route";
 import { issueRoute } from "./modules/issues/issue.route";
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
